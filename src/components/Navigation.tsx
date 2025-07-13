ae2b59f6f33a3cf4cb6584487451472f08d1755d
Zac Ching <zching9@gmail.com>
2025-07-13 12:51:49 -0400

    customize nav bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,17 +1,61 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navigation = () => {
+    const location = useLocation();
+    
     return (
         <nav>
             <h2>My Portfolio **CHANGE**</h2>
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/travel">Travel</Link></li>
-                <li><Link to="/photography">Photography</Link></li>
-                <li><Link to="/baseball">Baseball</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li>
+                    <Link
+                        to="/"
+                        className={location.pathname === "/" ? "active" : ""}
+                    >
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/about"
+                        className={location.pathname === "/about" ? "active" : ""}
+                    >
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/travel"
+                        className={location.pathname === "/travel" ? "active" : ""}
+                    >
+                        Travel
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/photography"
+                        className={location.pathname === "/photography" ? "active" : ""}
+                    >
+                        Photography
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/baseball"
+                        className={location.pathname === "/baseball" ? "active" : ""}
+                    >
+                        Baseball
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        to="/contact"
+                        className={location.pathname === "/contact" ? "active" : ""}
+                    >
+                        Contact
+                    </Link>
+                </li>
             </ul>
         </nav>
     );
